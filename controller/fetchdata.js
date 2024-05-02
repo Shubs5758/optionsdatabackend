575cf5761fbe873c22a2223cd9320c2bb4cf4974
SantoshAmbavaneShubham-at-686389543791 <shubham.satosh-ambavane@capgemini.com>
2024-05-02 23:07:44 +0530

    changes

--- controller/fetchdata.js
+++ controller/fetchdata.js
@@ -3,7 +3,7 @@ exports.fetchdata = async (req, res) => {
   const symbol = JSON.parse(req.query.symbol); // Get the symbol from the query parameters
   const strikeprice = JSON.parse(req.query.strikeprice); // Get the strike price from the query parameters
   console.log(strikeprice);
-  const expiryDate = req.query.expiryDate; // Get the expiry date from the query parameters
+  const expiryDate = JSON.parse(req.query.expiryDate); // Get the expiry date from the query parameters
   console.log(req.query.expiryDate);
 
   if (!symbol) {
@@ -11,23 +11,42 @@ exports.fetchdata = async (req, res) => {
   }
   const results = await Promise.all(
     symbol.map(async (symbol) => {
-      return await fetchOptionData.fetchOptionData(symbol);
+      const data = await fetchOptionData.fetchOptionData(symbol);
+      return { symbol, data };
     })
   );
   console.log(results);
   //   const data = await fetchOptionData.fetchOptionData(symbol);
+  const data1 = results.map(({ symbol, data }) => {
+    const CEData = data.CE.find(
+      (option) =>
+        option.strikePrice === strikeprice[0] &&
+        option.expiryDate === expiryDate[0]
+    );
+    const PEData = data.PE.find(
+      (option) =>
+        option.strikePrice === strikeprice[0] &&
+        option.expiryDate === expiryDate[0]
+    );
 
-  const CEData = results.CE.find(
-    (option) =>
-      option.strikePrice === strikeprice && option.expiryDate === expiryDate
-  );
-  const PEData = results.PE.find(
-    (option) =>
-      option.strikePrice === strikeprice && option.expiryDate === expiryDate
-  );
+    console.log(`CEData for ${symbol}: `, CEData);
+    console.log(`PEData for ${symbol}: `, PEData);
 
-  console.log(CEData);
-  console.log(PEData);
+    return { symbol, finaldata: [CEData, PEData] };
+  });
+
+  console.log("data1", data1);
+  //   const CEData = results.CE.find(
+  //     (option) =>
+  //       option.strikePrice === strikeprice && option.expiryDate === expiryDate
+  //   );
+  //   const PEData = results.PE.find(
+  //     (option) =>
+  //       option.strikePrice === strikeprice && option.expiryDate === expiryDate
+  //   );
+
+  //   console.log(CEData);
+  //   console.log(PEData);
   let finaldata = [CEData, PEData];
   if (finaldata) {
     res.status(200).json(finaldata);
